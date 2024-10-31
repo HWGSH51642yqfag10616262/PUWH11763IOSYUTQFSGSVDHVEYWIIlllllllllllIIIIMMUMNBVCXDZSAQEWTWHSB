423a144042b62bbd3575bccb2b6b1f6d2c2340ee
HWGSH51642yqfag10616262 <155875579+HWGSH51642yqfag10616262@users.noreply.github.com>
2024-10-31 07:08:33 +0300

    Update main.mm

--- main.mm
+++ main.mm
@@ -1,67 +1,6 @@
 
-#import <UIKit/UIKit.h>
+(((علاوي المنيوك AS7 الفرخ)))
 
-#define UIColorFromHEX(rgbValue) [UIColor \
-colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 \
-green:((float)((rgbValue & 0xFF00) >> 8))/255.0 \
-blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
-
-
-
-#import "main.h"
-
-#import "SCLAlertView/SCLAlertView.h"
-
-SCLAlertView *alert;
-
-
-@implementation NguyenNamFramework
-
-
-
-
-static NguyenNamFramework *active;
-
-//-----------------//
-
-+ (void)load
-{
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3* NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-
-alert = [[SCLAlertView alloc] initWithNewWindow];  
-
-        active =  [NguyenNamFramework new];
-        [active start];
-
-    });
-}
-
-
--(void)start{
-
-alert.shouldDismissOnTapOutside = YES;//Replace with NO if you want the user to force a button in the alert
-
-alert.customViewColor = UIColorFromHEX(0x474747);
-  
-
-alert.showAnimationType = SCLAlertViewShowAnimationSlideInToCenter;   
-
-alert.backgroundType = SCLAlertViewBackgroundBlur;
-
-alert.cornerRadius = 20.0f;
-
-alert.backgroundViewColor = UIColorFromHEX(0x302d26);
-
-
-
-//Button
-
-//Demo Button
-/*
-[alert addButton:@"Name Button" actionBlock:^(void) {
-[[UIApplication sharedApplication] openURL:[NSURL URLWithString: @"URL YOU WANT TO OPEN WHEN CLICK"] options:@{} completionHandler:nil];
-}];
-*/
 
 
 //Example button
